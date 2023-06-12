ba59960e262cf29035c0910921877e115de7738c
Benjamin Crozat <benjamincrozat@me.com>
2023-06-12 11:40:19 +0200

    WIP

--- database/migrations/2023_06_08_183003_create_sessions_table.php
+++ database/migrations/2023_06_08_183003_create_sessions_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -8,6 +9,8 @@ return new class extends Migration
 {
     public function up()
     {
+        DB::statement('SET SESSION sql_require_primary_key = 0');
+
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
@@ -16,6 +19,8 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
+
+        DB::statement('SET SESSION sql_require_primary_key = 1');
     }
 
     public function down()
